c0e19232c1b71ac531e1263c422889e742f8787d
Jorge Lopez <jorgelopez2906@gmail.com>
2019-05-07 11:50:00 -0500

    Initial Commit

---
+++ AlertifyIOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AlertifyIOS
+//
+//  Created by Jorge Lopez on 5/7/19.
+//  Copyright © 2019 Jorge Lopez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
