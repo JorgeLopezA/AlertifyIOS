f7e745cd3344d83738d3b36627b7a9cf3632ec30
luisferced <fernandocm_97@outlook.com>
2019-05-14 20:07:44 -0500

    Todo lit todo

--- AlertifyIOS/FirstViewController.swift
+++ AlertifyIOS/FirstViewController.swift
@@ -10,11 +10,64 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var alerta: UIImageView!
+    var pulseLayers = [CAShapeLayer]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func createPulse(){
+        for _ in 0...2 {
+            let circularPath = UIBezierPath(arcCenter: .zero, radius: UIScreen.main.bounds.size.width/2.0, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
+            let pulseLayer = CAShapeLayer()
+            pulseLayer.path = circularPath.cgPath
+            pulseLayer.lineWidth = 2.0
+            pulseLayer.fillColor = UIColor.clear.cgColor
+            pulseLayer.strokeColor = UIColor.orange.cgColor
+            pulseLayer.lineCap = CAShapeLayerLineCap.round
+            pulseLayer.position = CGPoint(x: alerta.frame.size.width/2.0, y: alerta.frame.size.width/2.0)
+            alerta.layer.addSublayer(pulseLayer)
+            pulseLayers.append(pulseLayer)
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.animatePulse(index: 0)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                self.animatePulse(index: 1)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    self.animatePulse(index: 2)
+                }
+            }
+        }
+    }
+    
+    func animatePulse(index: Int){
+        pulseLayers[index].strokeColor = UIColor.orange.cgColor
+        let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
+        scaleAnimation.duration = 2.0
+        scaleAnimation.fromValue = 0.0
+        scaleAnimation.toValue = 0.9
+        scaleAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        scaleAnimation.repeatCount = .greatestFiniteMagnitude
+        pulseLayers[index].add(scaleAnimation, forKey: "scale")
+        
+        let opacityAnimation = CABasicAnimation(keyPath: #keyPath(CALayer.opacity))
+        opacityAnimation.duration = 2.0
+        opacityAnimation.fromValue = 2.0
+        opacityAnimation.toValue = 0.0
+        opacityAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        opacityAnimation.repeatCount = .greatestFiniteMagnitude
+        pulseLayers[index].add(opacityAnimation, forKey: "opacity")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        alerta.layer.cornerRadius = alerta.frame.size.width/2.0
+        createPulse()
+        self.tabBarController?.delegate = self as? UITabBarControllerDelegate 
+    }
 
 }
 
--- AlertifyIOS/SecondViewController.swift
+++ AlertifyIOS/SecondViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import MapKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    let gps = CLLocationManager()
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        configurarMapa()
     }
-
+    
+    func configurarMapa(){
+        map.delegate = self
+        gps.delegate = self
+        gps.desiredAccuracy = kCLLocationAccuracyBest
+        gps.requestWhenInUseAuthorization()
+        // Zoom inicial
+        let centro = CLLocationCoordinate2DMake(19.5953, -99.2276)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: centro, span: span)
+        map.region = region
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus){
+        if status == .authorizedWhenInUse{
+            gps.startUpdatingLocation()
+            gps.startUpdatingHeading()
+        } else if status == .denied{
+            print("Permiso denegado")
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation){
+        map.setCenter(userLocation.coordinate, animated: true)
+        
+        //Pin sobre el mapa
+        let pin = MKPointAnnotation()
+        pin.coordinate = userLocation.coordinate
+        pin.title = "Intento de Robo"
+        pin.subtitle = "Tec CEM"
+        map.addAnnotation(pin)
+    }
 
 }
 
